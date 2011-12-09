f66004e43e12390fedbe3414e70066b81f991c36
Chakkrit Sripariwut <chakkrit@abctech-thailand.com>
2011-12-09 13:28:56 +0700

    build database auth.db

---
+++ db/authdb.sql
@@ -0,0 +1,18 @@
+CREATE TABLE roles (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+role TEXT UNIQUE
+);
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT UNIQUE,
+email TEXT,
+password TEXT
+);
+
+CREATE TABLE user_roles (
+user_id INTEGER REFERENCES users(id),
+role_id INTEGER REFERENCES roles(id),
+PRIMARY KEY(user_id, role_id)
+);
+
